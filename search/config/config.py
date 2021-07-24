7b1caaca0851794dbe771e82765b88f79db0dfb6
12345-mcpython <63326494+12345-mcpython@users.noreply.github.com>
2021-07-24 20:32:03 +0800

    Update config.py

--- search/config/config.py
+++ search/config/config.py
@@ -9,7 +9,7 @@ class Config:
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     MYSQL = dict(
         MYSQL_HOST=os.environ.get("MYSQL_HOST", "127.0.0.1"),
-        MYSQL_PORT=int(os.environ.get("MYSQL_PORT"), 3306),
+        MYSQL_PORT=int(os.environ.get("MYSQL_PORT", 3306)),
         MYSQL_USER=os.environ.get("MYSQL_USER", "root"),
         MYSQL_PASSWORD=os.environ.get("MYSQL_PASSWORD")
     )
