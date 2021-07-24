7c352c1da4004589a879700549181b0f562b3dac
12345-mcpython <63326494+12345-mcpython@users.noreply.github.com>
2021-07-24 12:46:03 +0800

    Add verbose

--- search/crawler/spider/mc_crawl.py
+++ search/crawler/spider/mc_crawl.py
@@ -10,6 +10,7 @@ from search.database.mysql import Simple
 from search.utils.parse_url import get_url_scheme, url_join
 
 mc = Client(["127.0.0.1:11211"], debug=True)
+verbose = False
 
 
 def disable_useless_url(url):
@@ -21,10 +22,12 @@ def disable_useless_url(url):
 
 
 def request(url):
-    print("Request url: ", url)
+    if verbose:
+        print("Request url: ", url)
     memcached_title = get_memcached(url)
     if memcached_title:
-        print("Memcached has cache. cache: " + memcached_title, ", url: " + url)
+        if verbose:
+            print("Memcached has cache. cache: " + memcached_title, ", url: " + url)
         a = test_title(url, memcached_title)
         return a
     req = requests.get(url, headers=Config.HEADERS, timeout=15)
@@ -34,7 +37,8 @@ def request(url):
 
 
 def request_clear(url):
-    print("Request url: ", url)
+    if verbose:
+        print("Request url: ", url)
     req = requests.get(url, headers=Config.HEADERS, timeout=15)
     req.encoding = req.apparent_encoding
     req.raise_for_status()
@@ -43,7 +47,8 @@ def request_clear(url):
 
 def test_execute_file(url):
     if ".exe" in url or ".zip" in url or ".rar" in url or '.7z' in url or '.jpg' in url or '.png' in url or '.jar' in url or '.webm' in url or '.webp' in url:
-        print("Request ignore: is a execute file or image file!")
+        if verbose:
+            print("Request ignore: is a execute file or image file!")
         return True
     return False
 
@@ -61,21 +66,25 @@ def test_title(url, text):
     if s.title:
         if "mc" in s.title.string.lower() or "minecraft" in s.title.string.lower() or "我的世界" in s.title.string:
             if "登录" not in s.title.string and "提示信息" not in s.title.string and "注册" not in s.title.string:
-                print("Check url successful: ", s.title.string.replace("\n", "").strip())
+                if verbose:
+                    print("Check url successful: ", s.title.string.replace("\n", "").strip())
                 save_memcached(url, s.title.string.strip().replace("\n", ""))
                 return s.title.string
             # print("提示信息" not in s.title.string.lower())
             else:
-                print("Request ignore: the page is login or register page: ",
-                      s.title.string)
+                if verbose:
+                    print("Request ignore: the page is login or register page: ",
+                          s.title.string)
                 save_memcached(url, s.title.string.strip().replace("\n", ""))
                 return False
         else:
-            print("Result ignore: mc element not in s.title.string: ", s.title.string)
+            if verbose:
+                print("Result ignore: mc element not in s.title.string: ", s.title.string)
             save_memcached(url, s.title.string.strip().replace("\n", ""))
             return False
     else:
-        print("Result ignore: s.title.string is None")
+        if verbose:
+            print("Result ignore: s.title.string is None")
         save_memcached(url, "null")
         return False
 
@@ -85,11 +94,13 @@ class MCCrawler:
         self.start = time.time()
         self.db = Simple(Config.MYSQL['MYSQL_HOST'], Config.MYSQL["MYSQL_PORT"], Config.MYSQL['MYSQL_USER'],
                          Config.MYSQL['MYSQL_PASSWORD'], "so")
+        self.verbose = True
         self.error_count = 0
         self.crawl_count = 0
         self.successful_count = 0
         self.duplicate_count = 0
         self.invalid_count = 0
+        self.base_count = 0
         self.error_list = []
 
     def get_info(self):
@@ -97,6 +108,7 @@ class MCCrawler:
 
     def crawl_url(self, url):
         try:
+            print("Base Request: " + str(self.base_count))
             base_url = url[2]
             scheme = get_url_scheme(base_url)
             r = request_clear(base_url)
@@ -106,9 +118,9 @@ class MCCrawler:
                 ls = []
                 if disable_useless_url(i):
                     result = url_join(scheme, i.get("href"))
-                    print("Parsing {} url:".format(self.crawl_count), result)
+                    if verbose:
+                        print("Parsing {} url:".format(self.crawl_count), result)
                     if self.test_in_database(result):
-                        self.invalid_count += 1
                         continue
                     if test_execute_file(result):
                         self.invalid_count += 1
@@ -122,13 +134,12 @@ class MCCrawler:
                         self.invalid_count += 1
                         continue
                     ls.append(title.strip().replace("\n", ""))
-                    print("Successful parse title:", title.replace("\n", "").strip())
+                    if verbose:
+                        print("Successful parse title:", title.replace("\n", "").strip())
                     ls.append(result)
                     res = self.db.insert_data("so_backup", ("title", "url"), tuple(ls))
                     if res == "-1":
                         print("Fail to insert data!")
-                    elif res >= 0:
-                        print("Successful insert data!")
                     self.successful_count += 1
         except Exception as e:
             import traceback
@@ -149,7 +160,8 @@ class MCCrawler:
             with open("error.txt", "w") as e:
                 for i in self.error_list:
                     e.write(str(i) + "\n\n")
-            print("Use time: " + str(round(use_time / 60, 2)) + "min")
+            print("Use time: " + str(round(use_time / 60, 2)) + "min", end="  ")
+            print(str(round(use_time, 2)) + "s")
             sys.exit(0)
 
     def test_in_database(self, url):
@@ -158,7 +170,8 @@ class MCCrawler:
             url1 = i[0]
             if url1 == url:
                 self.duplicate_count += 1
-                print("Have duplicate url! Duplicate count: ", end="")
+                if verbose:
+                    print("Have duplicate url! Duplicate count: ", end="")
                 print(self.duplicate_count)
                 return True
         return False
@@ -167,6 +180,7 @@ class MCCrawler:
         print("Start crawl.")
         a = self.get_info()
         for i in a:
+            self.base_count += 1
             self.crawl_url(i)
         use_time = time.time() - self.start
         print("\n" * 3)
